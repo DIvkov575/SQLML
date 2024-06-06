7a7ec55a1e07e2bc9824d6dd46fbc69e99c3c5c2
Divkov575 <dmitriy@ivkov.net>
2024-06-05 21:48:25 -0400

    working pgsql lin_regression.sql

--- lin_regression.sql
+++ lin_regression.sql
@@ -4,17 +4,20 @@ create or replace function lin_reg (in tbl regclass)
     returns table(slope float, intercept float)
     language plpgsql
 as $$
-    SELECT slope, y_max - x_max * slope as intercept
-    FROM (
-             SELECT
-                 SUM((x - x_bar) * (y - y_bar)) / SUM((x - x_bar) * (x - x_bar)) as slope,
-                 MAX(x_bar) as x_max,
-                 MAX(y_bar) as y_max
-             FROM (
-                      EXECUTE format("SELECT x, AVG(x)OVER() as x_bar, y, AVG(y)OVER() as y_bar FROM %s", tbl)
-                  )
-         );
-
+begin
+    return query execute format('
+        SELECT slope, y_max - x_max * slope as intercept
+        FROM (
+                 SELECT
+                     SUM((x - x_bar) * (y - y_bar)) / SUM((x - x_bar) * (x - x_bar)) as slope,
+                     MAX(x_bar) as x_max,
+                     MAX(y_bar) as y_max
+                 FROM (
+                       SELECT x, AVG(x)OVER() as x_bar, y, AVG(y)OVER() as y_bar FROM %s
+                      )
+             );
+         ', tbl);
+end
 $$;
 
 
